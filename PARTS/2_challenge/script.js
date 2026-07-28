f1b9f34f21800758877cc19223b69e542625114b
Asif Khan <asifjamshed4@gmail.com>
2026-07-28 19:29:40 +0530

    Practice

---
+++ PARTS/2_challenge/script.js
@@ -0,0 +1,30 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "real";
+const botLocation = "memory";
+const favoriteLanguage = "0 & 1";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact =
+  "Coding is doing on pen and paper first before doing " +
+  favoriteLanguage +
+  ".";
+console.log(codingFact);
+
+codingFact = "Now coding is hungry for " + favoriteLanguage + "language.";
+console.log(codingFact);
+
+codingFact =
+  "The low you can go " + favoriteLanguage + "in programming language.";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
---
+++ PARTS/3_challenge/script.js
@@ -0,0 +1,23 @@
+let adjective = "automate";
+let noun = "fun";
+let verb = "IDE";
+let place = "computer";
+let adjective2 = "fast";
+let noun2 = "editior";
+
+let firstStory;
+firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "swift";
+noun = "expo";
+verb = "JavaScript";
+place = "server";
+adjective2 = "native";
+noun2 = "Node.js";
+
+let secondStory;
+secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);

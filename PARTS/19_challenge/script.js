a1b97e1cb5832c74e694b8310c3bd3c7346428c2
Asif Khan <asifjamshed4@gmail.com>
2026-08-06 18:37:08 +0530

    Write function to check leap year

--- PARTS/19_challenge/script.js
+++ PARTS/19_challenge/script.js
@@ -1,1 +1,19 @@
+function isLeapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return `${year} is a leap year.`;
+      } else {
+        return `${year} is not a leap year.`;
+      }
+    } else {
+      return `${year} is a leap year.`;
+    }
+  } else {
+    return `${year} is not a leap year.`;
+  }
+}
 
+const year = 1900;
+const result = isLeapYear(year);
+console.log(result);

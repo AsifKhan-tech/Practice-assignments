dc7f812800d3b00380f30312c785ece5750f009c
Asif Khan <asifjamshed4@gmail.com>
2026-08-23 21:31:11 +0530

    JavaScript functionality for temperature amplitude calculation

---
+++ dev-skills/script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+// PROBLEM 1:
+//* We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+//What's temprature amplitude?
+//Answer: Difference between highest & lowest temprature in the array
+//How to calculate max & min tempratures?
+//What's a sensor error? And what to do
+
+// 2) Breaking up into sub-problems
+//How to ignore errors?
+//Find max value in temprature value in array
+//Find min value in temprature value in array
+//subtract min from max it's(amplitude) and then return it
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+
+    //* if (temps[i] > max) max = temps[i];
+    //* if (temps[i] < min) min = temps[i];
+    if (typeof currentTemp !== "number") continue;
+
+    if (currentTemp > max) max = currentTemp;
+    if (currentTemp < min) min = currentTemp;
+  }
+
+  console.log("Max: ", max);
+  console.log("Min: ", min);
+
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+
+console.log(amplitude);
+
+//Problem:2
+//* Function should now recieve 2 arrays of tempratures
+
+//* 1) Understanding the problem
+
+//* 2) Breaking up into sub-problems
